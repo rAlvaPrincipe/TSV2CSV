24418a4ad179e3f1440ddb9fdb12ea7bb99ad398
rAlvaPrincipe <r.alvaprincipe@campus.unimib.it>
2019-07-12 11:35:00 +0200

    1st v

---
+++ src/main/java/Main.java
@@ -0,0 +1,22 @@
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.sql.SparkSession;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {		
+		SparkSession session = SparkSession.builder().appName("ABSTAT-spark").master(args[0]).getOrCreate();
+		String dataset_file = args[1];
+		String output_dir = args[2];
+		
+		JavaRDD<String> input = session.read().textFile(dataset_file).javaRDD();
+
+		JavaRDD<String> csv = input.map(new Function<String, String>() {
+			public String call(String s) {
+				return s.replace("\t", ";");
+			}});
+		
+		csv.saveAsTextFile(output_dir);
+
+	}
+}
